7bebfc152017ae9269fca2640bd8c7946c9d5ab0
BenMusak <bomh19@student.aau.dk>
2020-04-20 15:45:59 +0200

    Added textures and greenhouse

--- include/tomato_plant.h
+++ include/tomato_plant.h
@@ -7,6 +7,7 @@ class TomatoPlant : public PlantBase
 public:
     float getHeight() override {return height;};
     int getNumTomatoes(){return num_tomatoes;};
+    float getGrowthRate(){return growth_rate;};
 
     void setHeight(double newHeight) {height = newHeight;}
     void grow(int days) override;
@@ -14,6 +15,9 @@ public:
 private:
     float height{10.0};
     int num_tomatoes{0};
-    int growth_rate{rand() % 5 + 1};
-    //
+
+    // Since we need float values, and rand() only returns integer values, a cheap and easy fix is needed
+    float currentRand = rand() % 300 + 100;
+    float growth_rate{currentRand / 100};
+    //rand() % 5 + 1
 };
--- src/tomato_example.cpp
+++ src/tomato_example.cpp
@@ -9,52 +9,93 @@
 #include <sstream>
 #include <vector>
 #include <iostream>
+#include <stdlib.h>
 
-void simulateDays(PlantBase &a_plant, Watertank &waterTank, int days, int waterNeedAmount){
+void simulateDays(PlantBase &a_plant, Watertank &waterTank, int days, int waterNeedAmount, int plantsAmount){
     
-    for (int i = 0; i < days; i++)
+    for (int i = 0; i < plantsAmount; i++)
     {
-        waterTank.emptying(waterNeedAmount + (a_plant.getHeight() / 10));
+        for (int i = 0; i < days; i++)
+        {
+            waterTank.emptying(waterNeedAmount + (a_plant.getHeight() / 10));
 
-        if(waterTank.getWaterAmount() <= 0)
-            waterTank.empty = true;
-        else
-            waterTank.empty = false;
-        
-        if(!waterTank.empty)
-            a_plant.grow(1);
-    }
+            if(waterTank.getWaterAmount() <= 0)
+                waterTank.empty = true;
+            else
+                waterTank.empty = false;
+            
+            if(!waterTank.empty)
+                a_plant.grow(1);
+            else break;
+        }
+    } 
+
 }
 
 int main(int argc, char const *argv[])
 {
+    // Set random number generation clock
+    srand(time(NULL));
+    
     // Define types
     TomatoPlant my_plant;
     TomatoPlant my_plant2;
+    TomatoPlant my_plant3;
     CucumberPlant my_cucumber_plant;
     Greenhouse my_greenhouse;
     Watertank my_watertank;
 
+    std::cout << "First plant: " << my_plant.getGrowthRate() << std::endl;
+    std::cout << "Second plant: " << my_plant2.getGrowthRate() << std::endl;
+    std::cout << "Third plant: " << my_plant3.getGrowthRate() << std::endl;
+
     // Variables
     int daysToWait = 1;
+    int plantsAmount = 3;
 
     // Set render window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Tomato Simulator");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
+    // Texture rendering
+    sf::Texture greenhouseTex;
+    if(!greenhouseTex.loadFromFile("C:/Users/ejer/Desktop/DrivhusKode/GrowPlant/pictures/Drivhus_2_v5.png")){
+        std::cout << "Couldn't load textures, check directory." << std::endl;
+    }
+    greenhouseTex.setSmooth(true);
+
+    sf::Texture stalk;
+    if(!stalk.loadFromFile("C:/Users/ejer/Desktop/DrivhusKode/GrowPlant/pictures/stalk.jpg")){
+        std::cout << "Couldn't load textures, check directory." << std::endl;
+    }
+    stalk.setSmooth(true);
+
+    const sf::Texture *pTexture = &stalk;
+
+    sf::Sprite greenhouseSprite;
+    greenhouseSprite.setTexture(greenhouseTex);
+    greenhouseSprite.setPosition(100, 70);
+    greenhouseSprite.setScale(0.8, 0.8);
+
+    
     // sfml clock for updating
     sf::Clock deltaClock;
 
     // Tomato plant
     sf::RectangleShape tomatoStalk {sf::Vector2f{5.0, -my_plant.getHeight()}};
-    tomatoStalk.setPosition(window.getSize().x / 2, window.getSize().y);
-    tomatoStalk.setFillColor(sf::Color{0, 255, 0});
+    tomatoStalk.setPosition(181, 289);
+    tomatoStalk.setTexture(pTexture);
     
     // Tomato plant 2
     sf::RectangleShape tomatoStalk2 {sf::Vector2f{5.0, -my_plant2.getHeight()}};
-    tomatoStalk2.setPosition( (window.getSize().x / 2) - 50, window.getSize().y);
-    tomatoStalk2.setFillColor(sf::Color{255, 0, 0});
+    tomatoStalk2.setPosition(284, 288);
+    tomatoStalk2.setTexture(pTexture);
+
+    // Tomato plant 3
+    sf::RectangleShape tomatoStalk3 {sf::Vector2f{5.0, -my_plant3.getHeight()}};
+    tomatoStalk3.setPosition(174, 488);
+    tomatoStalk3.setTexture(pTexture);
 
     while (window.isOpen()){
 
@@ -87,12 +128,16 @@ int main(int argc, char const *argv[])
 
             if(ImGui::Button("Wait the days")){
                 // First TomatoPlant
-                simulateDays(my_plant, my_watertank, daysToWait, 10);
+                simulateDays(my_plant, my_watertank, daysToWait, 10, plantsAmount);
                 tomatoStalk.setSize(sf::Vector2f{5.0, -my_plant.getHeight()});
 
                 // Second TomatoPlant
-                simulateDays(my_plant2, my_watertank, daysToWait, 10);
+                simulateDays(my_plant2, my_watertank, daysToWait, 10, plantsAmount);
                 tomatoStalk2.setSize(sf::Vector2f{5.0, -my_plant2.getHeight()});
+
+                // Third TomatoPlant
+                simulateDays(my_plant3, my_watertank, daysToWait, 10, plantsAmount);
+                tomatoStalk3.setSize(sf::Vector2f{5.0, -my_plant3.getHeight()});
             } 
 
             // Show water amount and add button for refill
@@ -104,14 +149,18 @@ int main(int argc, char const *argv[])
                 my_watertank.fillUp(1000);
             }
             ImGui::End();
+            
             // Show LED lights and their value, button for changing value.
 
         ImGui::End();
 
         window.clear(sf::Color::Black);
 
+        // Draw to screen
+        window.draw(greenhouseSprite);
         window.draw(tomatoStalk);
         window.draw(tomatoStalk2);
+        window.draw(tomatoStalk3);
 
         ImGui::SFML::Render(window);
 
