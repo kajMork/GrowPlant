7ee67f7310cc1a209784cca801a09e27b6635895
Christian Høgh <chagh19@student.aau.dk>
2020-04-20 10:50:10 +0200

    heyy motherfuckers

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Kaspe/Aalborg Universitet/Kasper personlig/Struktureret system- og produktudvikling (SSP) (ROB2) - AAL - F20/GrowPlant
+# Install script for directory: C:/Users/chris/OneDrive/Dokumenter/C++project/GrowPlant
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Kaspe/Aalborg Universitet/Kasper personlig/Struktureret system- og produktudvikling (SSP) (ROB2) - AAL - F20/GrowPlant/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/chris/OneDrive/Dokumenter/C++project/GrowPlant/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
