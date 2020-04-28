4d69beaa4dcd2ff4c67cf21561ed41b4e3fe8ff1
admin <56363524+MadsRossen@users.noreply.github.com>
2020-04-28 21:37:39 +0200

    Menu bar work, choose between tomato and cucumber

--- include/cucumber_plant.h
+++ include/cucumber_plant.h
@@ -6,14 +6,19 @@ class CucumberPlant : public PlantBase
 {
 
 public:
+    std::string getName() override {return name;};
     float getHeight() override {return height;};
     int getNumCucumbers(){return num_cucumbers;};
 
     void grow(int days) override;
+
+    int getMaxHeight() override {return max_Height;};
 private:
     double height{0.0};
     int num_cucumbers{0};
     int growth_rate{rand() % 3 + 1};
+    int max_Height{15};
+    std::string name = "Cucumber";
 };
 
 
--- include/plant_base.h
+++ include/plant_base.h
@@ -1,10 +1,16 @@
 #pragma once
+#include <string>
 class PlantBase
 {
 public:
     virtual float getHeight() = 0;
 
     virtual void grow(int days) = 0;
+
+    virtual int getMaxHeight() = 0;
+
+    virtual std::string getName();
+
 private:
     
 };
